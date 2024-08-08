ef1e4d7b396351f81997e4ecf6ad26e5e701b786
HazmiHan <farhanhazmi29@gmail.com>
2024-08-08 18:46:12 +0700

    Setting up the bottom navbar, home fragment, and news fragment

--- app/src/main/java/com/farhanhazmi/laporkorupsi/MainActivity.kt
+++ app/src/main/java/com/farhanhazmi/laporkorupsi/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.farhanhazmi.laporkorupsi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.farhanhazmi.laporkorupsi.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.news -> replaceFragment(NewsFragment())
+
+                else ->{
+
+                }
+            }
+            true
+
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
