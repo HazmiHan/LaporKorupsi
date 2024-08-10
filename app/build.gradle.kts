58e65d8cab6cc9937f0a1fb62c5f2346e6193178
HazmiHan <farhanhazmi29@gmail.com>
2024-08-10 22:10:15 +0700

    Add splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-analytics")
+
+//    Splashscreen
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
---
+++ app/src/main/java/com/farhanhazmi/laporkorupsi/splash.kt
@@ -0,0 +1,23 @@
+package com.farhanhazmi.laporkorupsi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()  // Close the SplashActivity
+        }, 2000)  // Duration of the splash screen in milliseconds (2000ms = 2s)
+    }
+}
